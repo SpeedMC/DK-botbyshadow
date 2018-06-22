5c6b6891ef7d12a0b58cfdac0257e9f9357bad51
Modo <39749105+ImHamoood@users.noreply.github.com>
2018-06-22 18:17:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1505,6 +1505,27 @@ Server name: __${guild.name}__
 Server owner: __${guild.owner}__**`)
 });
 
+client.on("message", (message) => {
+if (message.content.startsWith("-ct")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'text');
+message.channel.sendMessage('تـم إنـشاء روم كـتابـي')
+
+}
+});
+
+
+client.on("message", (message) => {
+if (message.content.startsWith("-cv")) {
+            if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
+        let args = message.content.split(" ").slice(1);
+    message.guild.createChannel(args.join(' '), 'voice');
+    message.channel.sendMessage('تـم إنـشاء روم صـوتي')
+    
+}
+});
+
 
 
 
